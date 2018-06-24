fe9fabcaefd9f1bfd9598da176f9f7bd5202942d
System Administrator <root@DMP.local>
2018-06-24 10:51:47 +0600

    setup dev dependecies & dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     return {
         entry: './src/app.js',
         output: {
-            path: path.join(__dirname, 'public'),
+            path: path.join(__dirname, 'public', 'dist'),
             filename: 'bundle.js'
         },
         optimization: {
@@ -49,7 +49,8 @@ module.exports = (env) => {
         devtool: env ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true
+            historyApiFallback: true,
+            publicPath: '/dist/'
         }
     }
 }
