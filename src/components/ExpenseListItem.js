c52c410121d7b79fd5f3ced7bfe380df1cba4382
System Administrator <root@DMP.local>
2018-06-25 09:23:45 +0600

    firebase added

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,13 +3,14 @@ import { removeExpense } from '../actions/expenses'
 import {Link} from 'react-router-dom'
 import { connect } from 'react-redux'
 import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
     <div>
         <Link to={`edit/${id}`}>
             <h4>{description}</h4>
         </Link>
-        <p>Amount: {amount}</p>
+        <p>Amount: {numeral(amount).format('$0,0.00')}</p>
         <p>Created At: {moment(createdAt).format('llll')}</p>
     </div>
 )
